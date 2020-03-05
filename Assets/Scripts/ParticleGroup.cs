f2b5d8319856376c7a490c4d563942e22f34d3bf
sh1ch <isogucchi-code@outlook.com>
2020-03-05 11:25:55 +0900

    feat: フレアのエフェクト（組み合わせ）を追加。

---
+++ Assets/Scripts/ParticleGroup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ParticleGroup : MonoBehaviour
+{
+    private ParticleSystem[] _Particles;
+
+    private int _Count => _Particles?.Length ?? 0;
+
+    private bool _IsInitialized = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _Particles = GetComponentsInChildren<ParticleSystem>();
+
+        _IsInitialized = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_Count <= 0 && _IsInitialized)
+        {
+            Destroy(gameObject);
+        }
+
+        bool canDestroy = _Particles.All(p => p == null || p.IsAlive(true) == false);
+
+        if (canDestroy)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/ParticleSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSpawner : MonoBehaviour
+{
+    public GameObject SpawnedParticlePrefab;
+    public Canvas OwnerCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            var clickedPosition = Input.mousePosition;
+            var newPrehub = Instantiate(SpawnedParticlePrefab);
+
+            Vector2 localPoint;
+            var canvasRect = OwnerCanvas.GetComponent<RectTransform>();
+
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRect, clickedPosition, OwnerCanvas.worldCamera, out localPoint);
+
+            newPrehub.transform.SetParent(this.transform, false);
+            var pos = newPrehub.GetComponent<RectTransform>().anchoredPosition;
+
+            newPrehub.GetComponent<RectTransform>().anchoredPosition = localPoint;
+            newPrehub.SetActive(true);
+        }
+    }
+}
